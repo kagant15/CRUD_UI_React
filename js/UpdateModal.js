995b845179bf2dfad388b4e3c7c1e68c7d9a4d6c
Thomas Kagan <tomkagan26@gmail.com>
2017-01-15 01:35:51 -0500

    Added prototypes updated callbacks for Immutable js

--- js/UpdateModal.js
+++ js/UpdateModal.js
@@ -11,6 +11,8 @@ var Col = require('react-bootstrap').Col;
 var FieldGroup = require('react-bootstrap').FieldGroup;
 var DatePicker = require("react-bootstrap-date-picker");
 
+var ImmutablePropTypes = require("react-immutable-proptypes");
+
 
 const UpdateModal = React.createClass({
 
@@ -27,7 +29,7 @@ const UpdateModal = React.createClass({
 		}
 	},
 
-	componentDidMount: function() {
+	componentDidMount() {
 
 	},
 
@@ -42,27 +44,27 @@ const UpdateModal = React.createClass({
 					<form>
 						<FormGroup controlId="formControlsFirstName">
 					      <ControlLabel>First Name</ControlLabel>
-					      <FormControl type="text" value={this.props.record.firstName} onChange={(event)=>{this.props.handleChange(event, "firstName")}} />
+					      <FormControl type="text" value={this.props.record.get('firstName')} onChange={(event)=>{this.props.handleChange(event, "firstName")}} />
 					    </FormGroup>
 						<FormGroup controlId="formControlsLastName">
 					      <ControlLabel>Last Name</ControlLabel>
-					      <FormControl type="text" value={this.props.record.lastName} onChange={(event)=>{this.props.handleChange(event, "lastName")}} />
+					      <FormControl type="text" value={this.props.record.get('lastName')} onChange={(event)=>{this.props.handleChange(event, "lastName")}} />
 					    </FormGroup>
 					    <FormGroup controlId="formControlsMiddleInitial">
 					      <ControlLabel>Middle Initial</ControlLabel>
-					      <FormControl type="text" value={this.props.record.middleInt} onChange={(event)=>{this.props.handleChange(event, "middleInt")}}/>
+					      <FormControl type="text" value={this.props.record.get('middleInt')} onChange={(event)=>{this.props.handleChange(event, "middleInt")}}/>
 					    </FormGroup>
 					    <FormGroup controlId="formControlsEmail">
 					      <ControlLabel>E-mail</ControlLabel>
-					      <FormControl type="email" value={this.props.record.email} onChange={(event)=>{this.props.handleChange(event, "email")}} />
+					      <FormControl type="email" value={this.props.record.get('email')} onChange={(event)=>{this.props.handleChange(event, "email")}} />
 					    </FormGroup>
 					    <FormGroup controlId="formControlsPhone">
 					      <ControlLabel>Phone Number</ControlLabel>
-					      <FormControl type="text"  value={this.props.record.phone} onChange={(event)=>{this.props.handleChange(event, "phone")}} />
+					      <FormControl type="text"  value={this.props.record.get('phone')} onChange={(event)=>{this.props.handleChange(event, "phone")}} />
 					    </FormGroup>
 					    <FormGroup controlId="formControlsSelect">
 					      <ControlLabel>Select</ControlLabel>
-					      <FormControl componentClass="select" value={this.props.record.position} onChange={(event)=>{this.props.handleChange(event, "position")}} >
+					      <FormControl componentClass="select" value={this.props.record.get('position')} onChange={(event)=>{this.props.handleChange(event, "position")}} >
 					      	<option value="select">select</option>
 					       	<option value="indirect">Indirect</option>
 			                <option value="direct">Direct</option>
@@ -73,13 +75,14 @@ const UpdateModal = React.createClass({
 						</FormGroup>
 						<FormGroup>
 							<ControlLabel>Date Hired</ControlLabel>
-							<DatePicker id="example-datepicker" />
+							<DatePicker id="example-datepicker" value={this.props.record.get('dateHired')} />
 						</FormGroup>
 					 </form>
 			    </Modal.Body>
 
 			    <Modal.Footer>
 			    	<Button bsStyle="primary" onClick={this.props.update}>Update</Button>
+			    	<Button bsStyle="primary" onClick={this.props.create}>Create</Button>
 			    	<Button onClick={this.props.cancel}>Cancel</Button>
 			    </Modal.Footer>
 
@@ -89,7 +92,11 @@ const UpdateModal = React.createClass({
 });
 
 UpdateModal.propTypes = {
-	handleChange: React.PropTypes.func
+	show : React.PropTypes.bool,
+	handleChange: React.PropTypes.func,
+	record: ImmutablePropTypes.map,
+	update: React.PropTypes.func,
+	cancel: React.PropTypes.func,
 }
 
 module.exports = UpdateModal;
