cacbd175cbff3b4beb92cb8ab635401f12317518
Thomas Kagan <tomkagan26@gmail.com>
2017-01-15 21:33:25 -0500

    some input field validation

--- js/UpdateModal.js
+++ js/UpdateModal.js
@@ -9,9 +9,47 @@ var HelpBlock = require('react-bootstrap').HelpBlock;
 var Form = require('react-bootstrap').Form;
 var Col = require('react-bootstrap').Col;
 var FieldGroup = require('react-bootstrap').FieldGroup;
+
 var DatePicker = require("react-bootstrap-date-picker");
 
 var ImmutablePropTypes = require("react-immutable-proptypes");
+
+
+const onlyLetters = (str)=> {
+	if(!str || str.length < 1){
+		return 'warning';
+	}
+	else if(/^[a-zA-Z]+$/.test(str)){
+		return 'success'
+	}
+	else{
+		return 'error'
+	}
+}
+
+const isEmail = (email)=> {
+	if(!email || email.length < 1){
+		return 'warning';
+	}
+	else if(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)){
+		return 'success'
+	}
+	else{
+		return 'error'
+	}
+}
+
+const isPhoneNumber = (phoneNumber)=> {
+	if(!phoneNumber || phoneNumber.length === 0){
+		// -- do nothing
+	}
+	else if(phoneNumber && phoneNumber.match(/\d/g).length===10){
+		return 'success';
+	}
+	else{
+		return 'error'
+	}
+}
 
 
 const UpdateModal = React.createClass({
@@ -42,47 +80,50 @@ const UpdateModal = React.createClass({
 
 				<Modal.Body>
 					<form>
-						<FormGroup controlId="formControlsFirstName">
+						<FormGroup controlId="formControlsFirstName" validationState={onlyLetters(this.props.record.get('firstName'))}>
 					      <ControlLabel>First Name</ControlLabel>
 					      <FormControl type="text" value={this.props.record.get('firstName')} onChange={(event)=>{this.props.handleChange(event, "firstName")}} />
+					      <FormControl.Feedback />
 					    </FormGroup>
-						<FormGroup controlId="formControlsLastName">
+						<FormGroup controlId="formControlsLastName" validationState={onlyLetters(this.props.record.get('lastName'))}>
 					      <ControlLabel>Last Name</ControlLabel>
 					      <FormControl type="text" value={this.props.record.get('lastName')} onChange={(event)=>{this.props.handleChange(event, "lastName")}} />
+					      <FormControl.Feedback />
 					    </FormGroup>
-					    <FormGroup controlId="formControlsMiddleInitial">
+					    <FormGroup controlId="formControlsMiddleInitial" validationState={onlyLetters(this.props.record.get('middleInt'))}>
 					      <ControlLabel>Middle Initial</ControlLabel>
-					      <FormControl type="text" value={this.props.record.get('middleInt')} onChange={(event)=>{this.props.handleChange(event, "middleInt")}}/>
+					      <FormControl type="text" maxLength={1} style={{width : 40}} value={this.props.record.get('middleInt')} onChange={(event)=>{this.props.handleChange(event, "middleInt")}}/>
 					    </FormGroup>
-					    <FormGroup controlId="formControlsEmail">
+					    <FormGroup controlId="formControlsEmail" validationState={isEmail(this.props.record.get('email'))}>
 					      <ControlLabel>E-mail</ControlLabel>
 					      <FormControl type="email" value={this.props.record.get('email')} onChange={(event)=>{this.props.handleChange(event, "email")}} />
+					      <FormControl.Feedback />
 					    </FormGroup>
-					    <FormGroup controlId="formControlsPhone">
+					    <FormGroup controlId="formControlsPhone" validationState={isPhoneNumber(this.props.record.get('phone'))}>
 					      <ControlLabel>Phone Number</ControlLabel>
 					      <FormControl type="text"  value={this.props.record.get('phone')} onChange={(event)=>{this.props.handleChange(event, "phone")}} />
+					      <FormControl.Feedback />
 					    </FormGroup>
 					    <FormGroup controlId="formControlsSelect">
-					      <ControlLabel>Select</ControlLabel>
+					      <ControlLabel>Position Category</ControlLabel>
 					      <FormControl componentClass="select" value={this.props.record.get('position')} onChange={(event)=>{this.props.handleChange(event, "position")}} >
-					      	<option value="select">select</option>
-					       	<option value="indirect">Indirect</option>
-			                <option value="direct">Direct</option>
-			                <option value="programManager">Program Manager</option>
-			                <option value="director">Director</option>
-			                <option value="executive">Executive</option>
+					      	<option value="Select">select</option>
+					       	<option value="Indirect">Indirect</option>
+			                <option value="Direct">Direct</option>
+			                <option value="Program Manager">Program Manager</option>
+			                <option value="Director">Director</option>
+			                <option value="Executive">Executive</option>
 					      </FormControl>
 						</FormGroup>
 						<FormGroup>
 							<ControlLabel>Date Hired</ControlLabel>
-							<DatePicker id="example-datepicker" value={this.props.record.get('dateHired')} />
+							<DatePicker id="example-datepicker" value={this.props.record.get('dateHired')} onChange={this.props.handleDateChange}/>
 						</FormGroup>
 					 </form>
 			    </Modal.Body>
 
 			    <Modal.Footer>
-			    	<Button bsStyle="primary" onClick={this.props.update}>Update</Button>
-			    	<Button bsStyle="primary" onClick={this.props.create}>Create</Button>
+			    {this.props.isUpdateModal ? <Button bsStyle="primary" onClick={this.props.update}>Update</Button> : <Button bsStyle="primary" onClick={this.props.create}>Create</Button>}
 			    	<Button onClick={this.props.cancel}>Cancel</Button>
 			    </Modal.Footer>
 
@@ -94,7 +135,7 @@ const UpdateModal = React.createClass({
 UpdateModal.propTypes = {
 	show : React.PropTypes.bool,
 	handleChange: React.PropTypes.func,
-	record: ImmutablePropTypes.map,
+	record: ImmutablePropTypes.map.isRequired,
 	update: React.PropTypes.func,
 	cancel: React.PropTypes.func,
 }
